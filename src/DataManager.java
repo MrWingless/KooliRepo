891bd971550c524477ca2102e0cf07e067a3e81a
MrWingless <wingless.valkyrie@gmail.com>
2015-03-07 13:13:31 +0200

    Renamed and Relocated some Parts
    Renamed the check that check if player has played before and Relocated
    Welcome message into that check (it appeared too late).

--- src/DataManager.java
+++ src/DataManager.java
@@ -1,16 +1,15 @@
 public class DataManager{
 
 	// Checks If The Player Already Exists
-	static void checkPlayer() throws Exception{
+	static void checkIfPlayerPlayedBefore() throws Exception{
 		java.io.File playerData = new java.io.File("/Players/"+ Player.getName() +".txt");
 		if (!playerData.exists()){
 			Player.setFirstGame(true);
 			playerData.createNewFile();
-			// Creates a new Player According to the Choice The Player Makes.
 		}
 		else{
 			Player.setFirstGame(false);
-			// Load The Data From the File!
 		}
+		PlayerMessage.welcomePlayer();
 	}
 }
--- src/GameMain.java
+++ src/GameMain.java
@@ -2,15 +2,24 @@ import java.util.Scanner;
 
 public class GameMain {
 	public static void main(String[] args) throws Exception{
+		System.out.println("You have started The Game Thing.");
+		System.out.println("This is a Console Based Turn Based Role Playing Game. CBTBRPG - In short.");
+		System.out.println("First Things First, We must find out if you've played before.");
+		System.out.println();
 		Scanner sisend = new Scanner(System.in);
 		System.out.println("Please Enter your Character name:");
 		Player.setName(sisend.next()); 
 		sisend.close();
 		
-		// Loo või loe mängija andmed failist
-		DataManager.checkPlayer();
+		// Kontrollb, kas mängija on varem mnänginud.
+		DataManager.checkIfPlayerPlayedBefore();
+		if (Player.getFirstGame()){
+			// Creates a new Player According to the Choice The Player Makes.
+		}
+		else{
+			// Load The Data From the File!
+		}
 		
-		PlayerMessage.welcomePlayer();
 		
 		
 		
