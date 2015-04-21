2aa532c38ce96f11009ae1e57aa94a5a2edb3f30
MargusM <margus.moru@gmail.com>
2015-04-22 01:48:27 +0300

    Menu
    Ei saand resolutsiooni vahetama, veel...
    Menüü osaliselt olemas, uusi asju nüüd kerge lisada sinna.
    Praegult läks kõik GameMain faili, aga ma pärast tõstan menüü värgi
    eraldi faili ümber. (eraldi klassid tegin GameMain alla menüü jaoks)

--- src/GameMain.java
+++ src/GameMain.java
@@ -1,16 +1,49 @@
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
-public class GameMain {
+
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.effect.GaussianBlur;
+import javafx.scene.effect.Glow;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+import javafx.animation.FadeTransition;import javafx.animation.TranslateTransition;
+
+
+
+public class GameMain extends Application {
 	public static Weapon[] weapons;
 	public static Armor[] armors;
 	public static Enemy[] enemies;
+	
+	public static int gameX = 800;
+	public static int gameY = 600;
+	
 	public static void main(String[] args) throws Exception{
-		System.out.println("You have started The Game Thing.");
+		//System.out.println("You have started The Game Thing.");
 		
 		// Loading Data:
 		weapons = DataManager.getWeapons();
 		armors = DataManager.getArmors();
 		enemies = DataManager.getEnemies();
+		
+		launch(args);
 		
 		/*
 		for (Weapon weapon : weapons){
@@ -23,6 +56,8 @@ public class GameMain {
 			armor.printStats();
 		}*/
 		
+		
+		/*
 		System.out.println("This is a Console Based Turn Based Role Playing Game. CBTBRPG - In short.");
 		System.out.println("First Things First, We must find out if you've played before.");
 		System.out.println();
@@ -52,6 +87,9 @@ public class GameMain {
 		System.out.println();
 		
 		//sword.printStats();
+		 
+		 
+		 */
 		
 		/*
 		do{}
@@ -61,4 +99,228 @@ public class GameMain {
 	public static void refreshEnemies() throws Exception{
 		enemies = DataManager.getEnemies();
 	}
+
+	private GameMenu gameMenu;
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+		Pane root = new Pane();
+		//root.setPrefSize(gameX, gameY);
+		
+		InputStream is = Files.newInputStream(Paths.get("Data/images/menu_bg.jpg"));
+		Image menu_bg = new Image(is);
+		is.close();
+		
+		ImageView menuBgView = new ImageView(menu_bg);
+		menuBgView.setFitWidth(gameX);
+		menuBgView.setFitHeight(gameY);
+		
+		gameMenu = new GameMenu();
+		
+		root.getChildren().addAll(menuBgView, gameMenu);
+		
+		Scene scene = new Scene(root);
+		primaryStage.setWidth(gameX);
+		primaryStage.setHeight(gameY);
+		primaryStage.setResizable(false);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	
+	
+	public void changeResolution() {
+		
+	}
+	
+	private class GameMenu extends Parent {
+		public GameMenu() {
+			VBox mainMenu = new VBox(15);
+			VBox loadMenu = new VBox(15);
+			VBox optionsMenu = new VBox(15);
+			VBox videoMenu = new VBox(15);
+
+			mainMenu.setTranslateX(gameX/5);
+			mainMenu.setTranslateY(gameY/4);
+			
+			loadMenu.setTranslateX(gameX);
+			loadMenu.setTranslateY(gameY/4);
+			
+			optionsMenu.setTranslateX(gameX);
+			optionsMenu.setTranslateY(gameY/4);
+			
+			videoMenu.setTranslateX(gameX);
+			videoMenu.setTranslateY(gameY/4);
+			
+			//Main Menu buttons
+			MenuButton btnMainNew = new MenuButton("New Player");
+			btnMainNew.setOnMouseClicked(event -> {
+
+			});
+			
+			MenuButton btnMainLoad = new MenuButton("Load Player");
+			btnMainLoad.setOnMouseClicked(event -> {
+
+			});
+			
+			MenuButton btnMainOptions = new MenuButton("Options");
+			btnMainOptions.setOnMouseClicked(event -> {
+				getChildren().add(optionsMenu);
+				//Move Options Menu away
+				TranslateTransition tt = new TranslateTransition(Duration.seconds(0.25), mainMenu);
+				tt.setToX(-gameX/4);
+				//Get Main Menu back
+				TranslateTransition tt1 = new TranslateTransition(Duration.seconds(0.5), optionsMenu);
+				tt1.setToX(gameX/5);
+				
+				tt.play();
+				tt1.play();
+				tt.setOnFinished(event1 -> getChildren().remove(mainMenu));
+				
+			});
+			
+			MenuButton btnMainExit = new MenuButton("Exit");
+			btnMainExit.setOnMouseClicked(event -> {
+				FadeTransition ft = new FadeTransition(Duration.seconds(0.5), this);
+				ft.setFromValue(1);
+				ft.setToValue(0);
+				ft.play();
+				
+				ft.setOnFinished(event1 -> System.exit(0));
+			});
+			
+			//Options Menu Buttons
+			
+			MenuButton low = new MenuButton("640x480");
+			MenuButton medium = new MenuButton("800x600");
+			MenuButton high = new MenuButton("1024x756");
+			
+			videoMenu.getChildren().addAll(low, medium, high);
+			
+			MenuButton btnOptVideo = new MenuButton("Video");
+			btnOptVideo.setOnMouseClicked(event -> {
+				getChildren().add(videoMenu);
+				
+				TranslateTransition tt1 = new TranslateTransition(Duration.seconds(0.5), videoMenu);
+				tt1.setToX(gameX*3/5);
+				tt1.play();
+				
+				low.setOnMouseClicked(event1 -> {
+					tt1.setToX(gameX);
+					tt1.play();
+					tt1.setOnFinished(event11 -> { getChildren().remove(videoMenu); });
+					gameX = 600;
+					gameY = 480;
+				});
+				
+				medium.setOnMouseClicked(event1 -> {
+					tt1.setToX(gameX);
+					tt1.play();
+					tt1.setOnFinished(event11 -> { getChildren().remove(videoMenu); });
+					gameX = 800;
+					gameY = 600;
+				});
+				
+				high.setOnMouseClicked(event1 -> {
+					tt1.setToX(gameX);
+					tt1.play();
+					tt1.setOnFinished(event11 -> { getChildren().remove(videoMenu); });
+					gameX = 1024;
+					gameY = 756;
+				});
+				
+			});
+			
+			MenuButton btnOptBack = new MenuButton("Back");
+			btnOptBack.setOnMouseClicked(event -> {
+				getChildren().add(mainMenu);
+				//Move Options Menu away
+				TranslateTransition tt = new TranslateTransition(Duration.seconds(0.25), optionsMenu);
+				tt.setToX(gameX);
+				//Get Main Menu back
+				TranslateTransition tt1 = new TranslateTransition(Duration.seconds(0.5), mainMenu);
+				tt1.setToX(gameX/5);
+				
+				tt.play();
+				tt1.play();
+				tt.setOnFinished(event1 -> getChildren().remove(optionsMenu));
+			});
+			
+			
+			
+			mainMenu.getChildren().addAll(btnMainNew, btnMainLoad, btnMainOptions, btnMainExit);
+			optionsMenu.getChildren().addAll(btnOptVideo, btnOptBack);
+			
+			Rectangle bg = new Rectangle(gameX, gameY);
+			bg.setFill(Color.GRAY);
+			bg.setOpacity(0.4);
+
+			getChildren().addAll(bg, mainMenu);
+			
+		}
+	}
+	
+	private static class MenuButton extends StackPane {
+
+		private Text text;
+		
+		public MenuButton(String name) {
+			text = new Text(name);
+			text.setFont(Font.font("Showcard Gothic", 15));
+			text.setFill(Color.WHITE);
+			text.setTranslateX(5);
+			
+			Rectangle bg = new Rectangle(200, 30);
+			bg.setOpacity(0.5);
+			bg.setFill(Color.BLACK);
+			bg.setEffect(new GaussianBlur(3.5));
+			
+			setAlignment(Pos.CENTER_LEFT);
+			
+			getChildren().addAll(bg, text);
+			
+			setOnMouseEntered(event -> {
+				bg.setTranslateX(10);
+				text.setFont(Font.font("Showcard Gothic", 18));
+				text.setTranslateX(20);
+				bg.setFill(Color.WHITE);
+				text.setFill(Color.BLACK);
+			});
+			
+			setOnMouseExited(event -> {
+				bg.setTranslateX(0);
+				text.setFont(Font.font("Showcard Gothic", 15));
+				text.setTranslateX(5);
+				bg.setFill(Color.BLACK);
+				text.setFill(Color.WHITE);
+			});
+			
+			DropShadow drop = new DropShadow(50, Color.WHITE);
+			drop.setInput(new Glow());
+			
+			setOnMousePressed(event -> setEffect(drop));
+			setOnMouseReleased(event -> setEffect(null));
+		}
+	}
+
+	
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
